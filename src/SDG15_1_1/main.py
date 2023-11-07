1c44fd067994ec738eeca6f8fe7e3a43265242ce
Ed Cuss <ed.cuss@ons.gov.uk>
2023-11-07 16:05:39 +0000

    updates to analysis

--- src/SDG15_1_1/main.py
+++ src/SDG15_1_1/main.py
@@ -6,9 +6,11 @@ def run(params: UserParams) -> None:
     gfr = SDG15_1_1('', params.root_dir, params.data_dir, params.output_dir)
 
     if params.single_year_test and all([params.lad_file_path, params.sam_file_path, params.nfi_file_path, params.year_start]):
+        print(f'Running single year export for year: {params.year_start}')
         gfr.calculate_sdg(params.lad_file_path, params.sam_file_path, params.nfi_file_path, params.year_start)
 
     if not params.single_year_test and all([params.year_start, params.year_end]):
+        print(f'Running multi year export for years: {params.year_start}-{params.year_end}')
         gfr.calculate_multiple_years(params.year_start, params.year_end)
 
     else:
